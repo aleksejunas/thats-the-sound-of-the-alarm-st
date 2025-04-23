e944d6943d680c403f4111059f444ac2ac9d77f5
aleksejunas <photo@seventyseven.no>
2025-04-23 22:38:22 +0200

    latest version of tailwind is up and running :D

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Tabs } from 'expo-router';
 import { Clock, Bell } from 'lucide-react-native';
 
@@ -12,23 +13,20 @@ export default function TabLayout() {
         },
         tabBarActiveTintColor: '#60a5fa',
         tabBarInactiveTintColor: '#666',
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Clock',
-          tabBarIcon: ({ size, color }) => (
-            <Clock size={size} color={color} />
-          ),
+          tabBarIcon: ({ size, color }) => <Clock size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="alarms"
         options={{
           title: 'Alarms',
-          tabBarIcon: ({ size, color }) => (
-            <Bell size={size} color={color} />
-          ),
+          tabBarIcon: ({ size, color }) => <Bell size={size} color={color} />,
         }}
       />
     </Tabs>
